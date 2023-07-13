ad64953293d6966f30d8c740c1415362dca133e0
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-07-13 15:47:44 -0700

    First API complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,73 @@
 import Header from "./components/Header"; // ✅
-import InputField from "./components/InputField";
-import Map from "./components/Map";
+import IpInformation from "./components/IpInformation";
+
+import { useState } from "react";
 
 function App() {
+  const [userIpAddress, setUserIpAddress] = useState("");
+  const [ip, setIp] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [timezone, setTimeZone] = useState("");
+  const [isp, setIsp] = useState("");
+
+  const handleOnChange = (e) => {
+    setUserIpAddress(e.target.value);
+  };
+
+  // Fetching the data
+  const handleFetchData = async () => {
+    try {
+      const url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_lfXFjIhblegGEefvocS4Ko7hkRWfM&ipAddress=${userIpAddress}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+      setIp(result.ip);
+      setCity(result.location.city);
+      setCountry(result.location.country);
+      setTimeZone(result.location.timezone);
+      setIsp(result.isp);
+    } catch (error) {
+      console.error("ERROR:", error);
+    }
+  };
+
   return (
     <>
-      <Header></Header>
-      <InputField></InputField>
-      <Map></Map>
+      <div className="bg-[url('/images/pattern-bg-desktop.png')] bg-cover bg-no-repeat h-[280px] flex flex-col items-center shadow-xl relative">
+        <Header></Header>
+        <div className="flex justify-center min-w-[496px]">
+          {/* Input Address or Domain */}
+          <div className="w-full">
+            <input
+              type="text"
+              placeholder="Search for any IP Adress or Domain"
+              className="py-4 px-7 rounded-lg outline-none text-black w-full rounded-r-none border"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div
+            className="rounded-r-lg flex flex-col justify-center items-center p-5 cursor-pointer w-14 h-fit"
+            style={{ backgroundColor: "hsl(0, 0%, 17%)" }}
+            onClick={handleFetchData}
+          >
+            <img
+              src="/images/icon-arrow.svg"
+              alt="Icon Arrow"
+              className="cursor-pointer w-7"
+            />
+          </div>
+        </div>
+        <div className="absolute bottom-20">
+          <IpInformation
+            ip={ip}
+            city={city}
+            country={country}
+            timezone={timezone}
+            isp={isp}
+          />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,70 +1,10 @@
 import React from "react";
-import { useState } from "react";
-// import FetchData from "./FetchData";
 
-const Header = ({ userIpAddress }) => {
-  userIpAddress = "Hello World";
-  const [ipAddress, setIpAddress] = useState("");
-  const [isValid, setIsValid] = useState(true);
-
-  // We need to get the input value into a state variable
-  const handleOnChange = (e) => {
-    const value = e.target.value;
-    setIsValid(validateIpAddress(value));
-    // setIpAddress(value);
-  };
-  // First Handle the errors the user might display
-  const validateIpAddress = (value) => {
-    const ipAddressRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
-    return ipAddressRegex.test(value);
-  };
-
-  const handleOnClick = () => {
-    if (isValid) {
-      console.log(ipAddress);
-      setIpAddress(ipAddress);
-    } else {
-      alert("Not Valid!");
-    }
-  };
-
-  const handleFetchData = (data) => {
-    console.log(data);
-  };
+const Header = () => {
   return (
     <>
-      <header className="w-full h-72 bg-[url('/public/images/pattern-bg-desktop.png')] bg-no-repeat bg-cover flex flex-col items-center pt-7 text-white shadow-lg">
-        <h1 className="font-bold text-3xl mb-4">IP Address Tracker</h1>
-        <div className="flex justify-center min-w-[496px]">
-          {/* Input Address or Domain */}
-          <div className="w-full">
-            <input
-              type="text"
-              placeholder="Search for any IP Adress or Domain"
-              className={`py-4 px-7 rounded-lg outline-none text-black w-full rounded-r-none ${
-                !isValid ? "border-2 border-red-500" : ""
-              }`}
-              onChange={handleOnChange}
-            />
-            {/* <FetchData ip={ipAddress} onFetchData={handleFetchData}></FetchData> */}
-            {!isValid && (
-              <p className="text-red-500 italic text-xs">
-                Please Enter A Valid IP Address
-              </p>
-            )}
-          </div>
-          <div
-            className="rounded-r-lg flex justify-center items-center p-5 cursor-pointer w-14 h-fit"
-            style={{ backgroundColor: "hsl(0, 0%, 17%)" }}
-          >
-            <img
-              src="/public/images/icon-arrow.svg"
-              alt="Icon Arrow"
-              className="cursor-pointer w-7"
-              onClick={handleOnClick}
-            />
-          </div>
-        </div>
+      <header className="text-center my-5 text-white font-bold text-3xl">
+        <h1>IP Tracker</h1>
       </header>
     </>
   );
